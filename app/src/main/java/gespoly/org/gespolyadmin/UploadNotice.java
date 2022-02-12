00e04bff701ef4d5392df156d7d89c5a5c0723dc
shyamsutar <shyamsutar629@gmail.com>
2022-02-12 11:45:27 +0530

    uploaded Notice section to Firebase

--- app/src/main/java/gespoly/org/gespolyadmin/UploadNotice.java
+++ app/src/main/java/gespoly/org/gespolyadmin/UploadNotice.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
@@ -28,14 +30,22 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.UUID;
 
+import gespoly.org.gespolyadmin.Model.NoticeModel;
 import gespoly.org.gespolyadmin.databinding.ActivityUploadNoticeBinding;
 
 public class UploadNotice extends AppCompatActivity {
 
     ActivityUploadNoticeBinding binding;
-    Bitmap bitmap;
+//    Bitmap bitmap;
     Uri imageUri;
+    DatabaseReference reference;
+    StorageReference storageReference;
+    String downloadUrl = "";
+    private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +54,103 @@ public class UploadNotice extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+        reference = FirebaseDatabase.getInstance().getReference();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        pd = new ProgressDialog(this);
+
+
+
+
         binding.selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 selectImage();
+            }
+        });
+
+
+        binding.uploadNoticeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.noticeTitle.getText().toString().isEmpty()){
+//                    Toast.makeText(UploadNotice.this, "Please Enter Notice Title", Toast.LENGTH_SHORT).show();
+                    binding.noticeTitle.setError("Empty");
+                    binding.noticeTitle.requestFocus();
+                }else if(imageUri == null){
+//                    if image not given
+                    uploadData();
+                }else{
+//                    if image given
+                    uploadImage();
+                }
+            }
+        });
+
+
+    }
+
+    private void uploadData() {
+
+        reference = reference.child("Notice");
+        final String uniqueKey = reference.push().getKey();
+
+
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("dd-MM-yy");
+        String date = currentDate.format(calForDate.getTime());
+
+        Calendar calForTime = Calendar.getInstance();
+        SimpleDateFormat currentTime = new SimpleDateFormat("hh:mm a");
+        String time = currentTime.format(calForTime.getTime());
+
+        NoticeModel noticeModel = new NoticeModel(binding.noticeTitle.toString(),downloadUrl,date,time,uniqueKey);
+
+        reference.child(uniqueKey).setValue(noticeModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                pd.dismiss();
+                Toast.makeText(UploadNotice.this, "Notice Uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(UploadNotice.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void uploadImage() {
+
+        pd.setMessage("Uploading...");
+        pd.show();
+
+        //to compress and upload
+        //here to compressing image OK
+        final StorageReference ref;
+//        final String randomKey = UUID.randomUUID().toString();
+        ref = storageReference.child("Notice");
+
+        ref.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(UploadNotice.this, "Cover photo saved", Toast.LENGTH_SHORT).show();
+                ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        downloadUrl = String.valueOf(uri);
+                        uploadData();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(UploadNotice.this, "Error...", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
