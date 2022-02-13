1830e2141604aaa172adfd74ddde9e0c8d92a46e
shyamsutar <shyamsutar629@gmail.com>
2022-02-13 12:43:39 +0530

    images uploaded to FireBase

--- app/src/main/java/gespoly/org/gespolyadmin/UploadImage.java
+++ app/src/main/java/gespoly/org/gespolyadmin/UploadImage.java
@@ -1,8 +1,10 @@
 package gespoly.org.gespolyadmin;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,7 +13,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import java.net.URI;
+import java.net.URL;
 
 import gespoly.org.gespolyadmin.databinding.ActivityUploadImageBinding;
 
@@ -20,12 +31,20 @@ public class UploadImage extends AppCompatActivity {
     ActivityUploadImageBinding binding;
     private String category;
     Uri imageUri;
+    ProgressDialog pd;
+    DatabaseReference reference;
+    StorageReference storageReference;
+    String downloadUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUploadImageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        pd = new ProgressDialog(this);
+        reference = FirebaseDatabase.getInstance().getReference().child("gallery");
+        storageReference = FirebaseStorage.getInstance().getReference().child("gallery");
 
         String[] items = new String[]{"Select Category", "Computer Department","Mechanical Department","Electrical Department", "Civil Department"};
         binding.imageCatergory1.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,items));
@@ -53,6 +72,73 @@ public class UploadImage extends AppCompatActivity {
                 startActivityForResult(intent,2);
             }
         });
+
+        binding.uploadImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(imageUri == null){
+                    Toast.makeText(UploadImage.this, "Please Select the Image", Toast.LENGTH_SHORT).show();
+                }else if (category.equals("Select Category")){
+                    Toast.makeText(UploadImage.this, "Please select image Category", Toast.LENGTH_SHORT).show();
+                }else{
+                    pd.setMessage("Uploading");
+                    pd.show();
+                    uploadImage();
+                }
+            }
+        });
+
+    }
+
+    private void uploadImage() {
+
+
+        //to compress and upload
+        //here to compressing image OK
+        final StorageReference ref1;
+//        final String randomKey = UUID.randomUUID().toString();
+        ref1 = storageReference.child("images");
+
+        ref1.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(UploadImage.this, "Cover photo saved", Toast.LENGTH_SHORT).show();
+                ref1.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        downloadUrl = String.valueOf(uri);
+                        uploadData();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(UploadImage.this, "Error...", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            private void uploadData() {
+                reference = reference.child(category);
+                final String uniqueKey = reference.push().getKey();
+                reference.child(uniqueKey).setValue(downloadUrl).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        pd.dismiss();
+                        Toast.makeText(UploadImage.this, "Image Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(UploadImage.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(UploadImage.this, "Something Went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
 
     }
 
